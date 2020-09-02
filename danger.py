6b56f5c2df0103947421d1578a76e4eb6aab418a
ayetatarbl <70614681+ayetatarbl@users.noreply.github.com>
2020-09-02 08:02:13 +0300

    Update danger.py

--- danger.py
+++ danger.py
@@ -5,7 +5,7 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^.nl(?: |$)(.*)')
+@register(outgoing=True, pattern='^.danger(?: |$)(.*)')
 async def typewriter(typew):
  message = typew.pattern_match.group(1)
  await typew.edit("Кхм-кхм...")
